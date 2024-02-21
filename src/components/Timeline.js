e1c1d5c16405586ac8c5f5566125a696fb425e4b
Jiros <jamie@munro.consulting>
2024-02-21 23:41:27 +0100

    timeline

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -2,37 +2,141 @@ import React from 'react';
 import { StaticImage } from 'gatsby-plugin-image';
 import './Timeline.css'; // Importing CSS styles
 
-// Example data for the timeline. Replace with your actual data.
-const timelineData = [
-  {
-    id: 1,
-    title: "Event 1",
-    text: "Description for Event 1. This paragraph will elaborate on the event's significance.",
-    image: "../images/pool.png", // Replace with your image path
-  },
-  {
-    id: 2,
-    title: "Event 2",
-    text: "Description for Event 2. This paragraph will elaborate on the event's significance.",
-    image: "../images/event2.png", // Replace with your image path
-  },
-  // Add more events as needed
-];
-
 const Timeline = () => (
   <div className="timeline-container">
-    {timelineData.map((event, index) => (
-      <div key={event.id} className={`timeline-item ${index % 2 === 0 ? 'normal' : 'reverse'}`}>
-        <div className="timeline-content">
-          <h2>{event.title}</h2>
-          <p>{event.text}</p>
-        </div>
-        <div className="timeline-image">
-          <StaticImage src={event.image} alt={event.title} />
-          {/* For dynamic images, use GatsbyImage component with GraphQL query */}
-        </div>
+    {/* January */}
+    <div className="timeline-item normal">
+      <div className="timeline-content">
+        <h2 class="windsong">January</h2>
+        <p class="fell">Georgia Islands: Witness juvenile king penguins in their natural habitat.</p>
       </div>
-    ))}
+      <div className="timeline-image">
+        {/* Replace 'path/to/image2.png' with your actual image path */}
+        <StaticImage class="month" src="../images/january.png" alt="Event 2" />
+      </div>
+    </div>
+    {/* February */}
+    <div className="timeline-item reverse">
+      <div className="timeline-content">
+        <h2 class="windsong">February</h2>
+        <p class="fell">Venice, Italy: Experience the romantic charm of this iconic city.</p>
+      </div>
+      <div className="timeline-image">
+        {/* Replace 'path/to/image2.png' with your actual image path */}
+        <StaticImage class="month" src="../images/february.png" alt="Event 2" />
+      </div>
+    </div>
+    {/* March */}
+    <div className="timeline-item normal">
+      <div className="timeline-content">
+        <h2 class="windsong">March</h2>
+        <p class="fell">Himeji, Japan: Explore the stunning Himeji Castle and its beautiful gardens.</p>
+      </div>
+      <div className="timeline-image">
+        {/* Replace 'path/to/image2.png' with your actual image path */}
+        <StaticImage class="month" src="../images/march.png" alt="Event 2" />
+      </div>
+    </div>
+    {/* April */}
+    <div className="timeline-item reverse">
+      <div className="timeline-content">
+        <h2 class="windsong">April</h2>
+        <p class="fell">Cape Town, South Africa: Enjoy the scenic beauty, vibrant culture, and delicious cuisine.</p>
+      </div>
+      <div className="timeline-image">
+        {/* Replace 'path/to/image2.png' with your actual image path */}
+        <StaticImage class="month" src="../images/april.png" alt="Event 2" />
+      </div>
+    </div>
+        {/* May */}
+        <div className="timeline-item normal">
+      <div className="timeline-content">
+        <h2 class="windsong">May</h2>
+        <p class="fell">Cudillero, Spain: Discover the idyllic seaside town with colourful houses.</p>
+      </div>
+      <div className="timeline-image">
+        {/* Replace 'path/to/image2.png' with your actual image path */}
+        <StaticImage class="month" src="../images/may.png" alt="Event 2" />
+      </div>
+    </div>
+    {/* June */}
+    <div className="timeline-item reverse">
+      <div className="timeline-content">
+        <h2 class="windsong">June</h2>
+        <p class="fell">Lofoten, Norway: Immerse yourself in the breathtaking landscapes and midnight sun.</p>
+      </div>
+      <div className="timeline-image">
+        {/* Replace 'path/to/image2.png' with your actual image path */}
+        <StaticImage class="month" src="../images/june.png" alt="Event 2" />
+      </div>
+    </div>
+    {/* July */}
+    <div className="timeline-item normal">
+      <div className="timeline-content">
+        <h2 class="windsong">July</h2>
+        <p class="fell">Carvoeiro, Portugal: Relax on picturesque beaches and explore hidden caves.</p>
+      </div>
+      <div className="timeline-image">
+        {/* Replace 'path/to/image2.png' with your actual image path */}
+        <StaticImage class="month" src="../images/july.png" alt="Event 2" />
+      </div>
+    </div>
+    {/* August */}
+    <div className="timeline-item reverse">
+      <div className="timeline-content">
+        <h2 class="windsong">August</h2>
+        <p class="fell">Tanzania: Witness the wildebeest river crossing during the Great Migration..</p>
+      </div>
+      <div className="timeline-image">
+        {/* Replace 'path/to/image2.png' with your actual image path */}
+        <StaticImage class="month" src="../images/august.png" alt="Event 2" />
+      </div>
+    </div>
+        {/* September */}
+        <div className="timeline-item normal">
+      <div className="timeline-content">
+        <h2 class="windsong">September</h2>
+        <p class="fell">Galápagos Islands, Ecuador: Experience the unique environment and wildlife of the Enchanted Isles.  </p>
+      </div>
+      <div className="timeline-image">
+        {/* Replace 'path/to/image2.png' with your actual image path */}
+        <StaticImage class="month" src="../images/september.png" alt="Event 2" />
+      </div>
+    </div>
+    {/* October */}
+    <div className="timeline-item reverse">
+      <div className="timeline-content">
+        <h2 class="windsong">October</h2>
+        <p class="fell">Ningaloo Reef, Australia: Surround yourself with colour and exotic sealife.</p>
+      </div>
+      <div className="timeline-image">
+        {/* Replace 'path/to/image2.png' with your actual image path */}
+        <StaticImage class="month" src="../images/october.png" alt="Event 2" />
+      </div>
+    </div>
+    {/* November */}
+    <div className="timeline-item normal">
+      <div className="timeline-content">
+        <h2 class="windsong">November</h2>
+        <p class="fell">Troll Peninsula, Iceland: Drive the breathtaking coastline under the Northern Lights.</p>
+      </div>
+      <div className="timeline-image">
+        {/* Replace 'path/to/image2.png' with your actual image path */}
+        <StaticImage class="month" src="../images/november.png" alt="Event 2" />
+      </div>
+    </div>
+    {/* December */}
+    <div className="timeline-item reverse">
+      <div className="timeline-content">
+        <h2 class="windsong">December</h2>
+        <p class="fell">Salzburg, Austria: Festive charm and Christmas Markets and The Sound of Music.</p>
+      </div>
+      <div className="timeline-image">
+        {/* Replace 'path/to/image2.png' with your actual image path */}
+        <StaticImage class="month" src="../images/december.png" alt="Event 2" />
+      </div>
+    </div>
+    {/* Repeat the structure for more events, alternating the className between 'normal' and 'reverse' */}
   </div>
 );
 
